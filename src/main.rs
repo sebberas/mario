6b5be2c21b8553f4c53cd2d7483967ce7a7a8b7c
Bech1337 <sebberas2@gmail.com>
2023-03-07 13:37:59 +0100

    basic window and scene structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::time::Duration;
+
+use sdl2::event::*;
+use sdl2::keyboard::*;
+
+use self::scene::*;
+
+mod scene;
+
+struct Game {
+    scene: Scene,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl2::init().unwrap();
+    let mut event_pump = sdl.event_pump().unwrap();
+    let video = sdl.video().unwrap();
+    let audio = sdl.audio().unwrap();
+
+    let window = video.window("Super Mario Bros", 800, 600).build().unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.present();
+    }
 }
