92638568cfda5ea151d774731b08eb8983c6d6c4
HectorSchreiner <agenthector007@gmail.com>
2023-03-28 14:06:30 +0200

    worked on input input_handler

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -1,8 +1,24 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
-pub struct InputHandler;
+pub struct InputHandler {
+    mouse_state: sdl2::mouse::MouseState,
+}
 
 impl InputHandler {
+    pub fn new(&mut self, mouse_state: sdl2::mouse::MouseState) -> InputHandler {
+        InputHandler { mouse_state }
+    }
 
+    pub fn is_mouse_clicked(&self) -> bool {
+        todo!()
+    }
+
+    pub fn is_mouse_held(&self) -> bool {
+        if self.is_mouse_clicked() {
+            return false;
+        }
+
+        true
+    }
 }
